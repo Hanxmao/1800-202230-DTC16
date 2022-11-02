f150b0f03400cc5c22c590a08dc6164e8981b355
Paolo Salvatierra <psalvatierra@my.bcit.ca>
2022-11-02 11:03:29 -0700

    Create Firebase API template

---
+++ scripts/firebaseAPI_template.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyC1X4r_TKjSOMFAzpFeIDTSl9wrYKJOQ10",
+    authDomain: "comp1800-202230-dtc16.firebaseapp.com",
+    projectId: "comp1800-202230-dtc16",
+    storageBucket: "comp1800-202230-dtc16.appspot.com",
+    messagingSenderId: "469001480013",
+    appId: "1:469001480013:web:4af3bdfd5632920ff23904"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
