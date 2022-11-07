1bb7bd638385997efa7ff475c66e18ef64e2d44c
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-07 09:58:54 -0800

    Make the reviews and rating random numbers, and fix some bugs

--- scripts/card_list.js
+++ scripts/card_list.js
@@ -8,7 +8,11 @@ function displayCards(collection) {
                 var description = doc.data().description;   
 				var cardID = doc.data().code;    
                 let newcard = cardTemplate.content.cloneNode(true);
+                let randReview = Math.floor(Math.random() * 120)
+                let randRating = Math.ceil(Math.random() * 5)
 
+                newcard.querySelector('#rating').innerHTML = randRating;
+                newcard.querySelector('#review').innerHTML = randReview;
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('#description').innerHTML = description;
                 newcard.querySelector('#card-img').src = `./images/card_img${cardID[cardID.length-1]}.svg`; 
