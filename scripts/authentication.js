2707314a6adc37effa26ec08c4be0cb9401d3f55
Paolo Salvatierra <psalvatierra@my.bcit.ca>
2022-11-08 20:46:38 -0800

    Update default profile info

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -12,8 +12,11 @@ var uiConfig = {
                 db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                     name: user.displayName,                    //"users" collection
                     email: user.email,                         //with authenticated user's ID (user.uid)
+                    phone: "",
                     country: "Canada",                      //optional default profile info      
-                    school: "BCIT"                          //optional default profile info
+                    city: "",                          //optional default profile info
+                    province: "",
+                    postal_code: "",
                 }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("main.html");       //re-direct to main.html after signup
