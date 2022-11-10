b74aad5b016087fe94d9766172de9966cfa58ff0
xuan1939 <113393025+xuan1939@users.noreply.github.com>
2022-11-10 00:15:48 -0800

    edited review.html

--- scripts/review.js
+++ scripts/review.js
@@ -1,8 +1,80 @@
+var cardReview = localStorage.getItem("cardReview"); //visible to all functions on this page
+
+function getReviewName(ReviewName) {
+  db.collection("hikes")
+    .where("code", "==", ReviewName)
+    .get()
+    .then((queryHike) => {
+      //see how many items are returned from the query with ".size"
+      size = queryHike.size;
+      // get the documents returned from query with ".docs"
+      hikes = queryHike.docs;
+
+      // We want to have one document per hike, so if the the result of
+      //the query is more than one, we can check it right now and clean the DB if needed.
+      if ((size = 1)) {
+        var thisHike = hikes[0].data();
+        name = thisHike.name;
+        document.getElementById("ReviewName").innerHTML = name;
+      } else {
+        console.log("Query has more than one data");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+}
+
+function writeReview() {
+  let Title = document.getElementById("title").value;
+  let Level = document.getElementById("level").value;
+  let Refer = document.getElementById("refer").value;
+  let Description = document.getElementById("description").value;
+
+  console.log(Title, Level, Refer, Description);
+}
+
+function writeReview() {
+  console.log("inside write review");
+  let Title = document.getElementById("title").value;
+  let Level = document.getElementById("level").value;
+  let Refer = document.getElementById("season").value;
+  let Description = document.getElementById("description").value;
+
+  console.log(Title, Level, Refer, Description);
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        var userEmail = userDoc.data().email;
+        db.collection("reviews")
+          .add({
+            code: cardReview,
+            userID: userID,
+            title: Title,
+            level: Level,
+            refer: Refer,
+            description: Description,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          })
+          .then(() => {
+            window.location.href = "thanks.html"; //new line added
+          });
+      });
+    } else {
+      // No user is signed in.
+    }
+  });
+}
+
 var hikeID = localStorage.getItem("hikeID"); //visible to all functions on this page
 
-function getHikeName(hikeCode) {
+function getHikeName(ReviewName) {
   db.collection("hikes")
-    .where("code", "==", hikeCode)
+    .where("code", "==", ReviewName)
     .get()
     .then((queryHike) => {
       //see how many items are returned from the query with ".size"
@@ -24,59 +96,4 @@ function getHikeName(hikeCode) {
       console.log("Error getting documents: ", error);
     });
 }
-getHikeName(hikeID);
-
-function writeReview() {
-  let Title = document.getElementById("title").value;
-  let Level = document.getElementById("level").value;
-  let Season = document.getElementById("season").value;
-  let Description = document.getElementById("description").value;
-
-  let Flooded = document.querySelector('input[name="flooded"]:checked').value;
-  let Scrambled = document.querySelector(
-    'input[name="scrambled"]:checked'
-  ).value;
-
-  console.log(Title, Level, Season, Description, Flooded, Scramble);
-}
-
-function writeReview() {
-  console.log("inside write review");
-  let Title = document.getElementById("title").value;
-  let Level = document.getElementById("level").value;
-  let Season = document.getElementById("season").value;
-  let Description = document.getElementById("description").value;
-  let Flooded = document.querySelector('input[name="flooded"]:checked').value;
-  let Scrambled = document.querySelector(
-    'input[name="scrambled"]:checked'
-  ).value;
-  console.log(Title, Level, Season, Description, Flooded, Scrambled);
-
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      var currentUser = db.collection("users").doc(user.uid);
-      var userID = user.uid;
-      //get the document for current user.
-      currentUser.get().then((userDoc) => {
-        var userEmail = userDoc.data().email;
-        db.collection("reviews")
-          .add({
-            code: hikeID,
-            userID: userID,
-            title: Title,
-            level: Level,
-            season: Season,
-            description: Description,
-            flooded: Flooded,
-            scrambled: Scrambled,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-          })
-          .then(() => {
-            window.location.href = "thanks.html"; //new line added
-          });
-      });
-    } else {
-      // No user is signed in.
-    }
-  });
-}
+getReview(cardReview);
