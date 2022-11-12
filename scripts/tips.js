86df7a868f99763980f3781a8df6e07a0e30ce8e
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-12 15:03:20 -0800

    write tips data to tips collection by JSON, and be able to present data to tips.html

---
+++ scripts/tips.js
@@ -0,0 +1,44 @@
+function displayTips(collection) {
+    db.collection(collection).get()
+        .then(snap => {
+            i = 1
+            snap.forEach(doc => { 
+                var title = doc.data().title;   
+                console.log(title);   
+                var description = doc.data().description
+
+                document.querySelector(`#title${i}`).innerHTML = title;
+                document.querySelector(`#description${i}`).innerHTML = description;
+                i++
+            }
+            )
+        })
+}
+
+displayTips("tips");
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------write tips data from tips.csv--------------------------
+async function getJSONdata() {
+    const response = await fetch('./tips.json'); //send get request
+    const data = await response.text();      //get file response
+    const tips = JSON.parse(data)
+
+    tips["tips"].forEach(tip => {
+        const title = tip.title;     //get tips title
+        const description = tip.description;        //tips description
+        db.collection('tips').add({     //write data to firebase
+            title:title,
+            description:description
+        })
+    })
+}
