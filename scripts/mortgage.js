a6921b7182f7b948dd4ffc97f3df2271dbd0badd
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-18 11:10:19 -0800

    mortgage page back end AJAX part done

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -2,20 +2,18 @@
 
 setup = () => {
     $("#convert").click(() => {
-        let fromCurrency = $('#fromCurrency').val()
-        let toCurrency = $("#toCurrency").val()
-        let amount = $('#amount').val()
-        // console.log("convert button");
-        // console.log(fromCurrency);
-        // console.log(toCurrency);
-        // console.log(amount);
+        let home_value = $('#home_value').val()
+        let downpayment = $("#downpayment").val()
+        let interest_rate = $('#interest_rate').val()
+        let duration_years = $('#duration_years').val()
         $.ajax({
             method: 'GET',
-            url: 'https://api.api-ninjas.com/v1/mortgagecalculator?loan_amount=200000&interest_rate=3.5&duration_years=30',
+            url: `https://api.api-ninjas.com/v1/mortgagecalculator?loan_amount=${home_value}&downpayment=${downpayment}&interest_rate=${interest_rate}&duration_years=${duration_years}`,
             headers: { 'X-Api-Key': 'bqWH6covDtf0E84LhDpQqw==D0VqpsDFkGSWCC98' },
             contentType: 'application/json',
             success: function (result) {
                 console.log(result);
+                $('#result').html('Monthly payment: $' + result.monthly_payment["total"] + '<br>Annual payment: $' + result.annual_payment['total'])
             },
             error: function ajaxError(jqXHR) {
                 console.error('Error: ', jqXHR.responseText);
