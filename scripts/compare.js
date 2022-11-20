bbba56da23fbe417565e2e2a3f197dba6aad99f6
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-20 11:12:31 -0800

    present the data to the table

--- scripts/compare.js
+++ scripts/compare.js
@@ -17,8 +17,14 @@ $('#cardtype1').change(()=>{
         interest_rate1 = firstCard.interest_rate
         welcome_bonus1 = firstCard.welcome_bonus
         extra_fee1 = firstCard.extra_fee
-        console.log(extra_fee1);
-        
+        bank1 = firstCard.bank
+
+        document.getElementById('name1').innerHTML = cardName1
+        document.getElementById('bank1').innerHTML = bank1
+        document.getElementById('annual_fee1').innerHTML = annual_fee1
+        document.getElementById('interest_rate1').innerHTML = interest_rate1
+        document.getElementById('welcome_bonus1').innerHTML = welcome_bonus1
+        document.getElementById('extra_fee1').innerHTML = extra_fee1
 
     })
 }) 
@@ -32,7 +38,14 @@ $('#cardtype2').change(()=>{
         interest_rate2 = secondCard.interest_rate
         welcome_bonus2 = secondCard.welcome_bonus
         extra_fee2 = secondCard.extra_fee
-        console.log(extra_fee2);
+        bank2 = secondCard.bank
 
+
+        document.getElementById('name2').innerHTML = cardName2
+        document.getElementById('bank2').innerHTML = bank2
+        document.getElementById('annual_fee2').innerHTML = annual_fee2
+        document.getElementById('interest_rate2').innerHTML = interest_rate2
+        document.getElementById('welcome_bonus2').innerHTML = welcome_bonus2
+        document.getElementById('extra_fee2').innerHTML = extra_fee2
     })
 }) 
