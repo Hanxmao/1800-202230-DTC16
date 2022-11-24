0f4c31136f9a3b9b2727d606a334ff2c462b439c
Hanxiao Mao <hanxmao@hotmail.com>
2022-11-23 20:38:31 -0800

    fix currency page login bug

--- scripts/currency.js
+++ scripts/currency.js
@@ -1,46 +1,47 @@
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        setup = () => {
-            $("#convert").click(() => {
-                let fromCurrency = $('#fromCurrency').val()
-                let toCurrency = $("#toCurrency").val()
-                let amount = $('#amount').val()
-                // console.log("convert button");
-                // console.log(fromCurrency);
-                // console.log(toCurrency);
-                // console.log(amount);
-                $.ajax({
-                    method: 'GET',
-                    url: `https://api.api-ninjas.com/v1/convertcurrency?want=${fromCurrency}&have=${toCurrency}&amount=${amount}`,
-                    headers: { 'X-Api-Key': 'bqWH6covDtf0E84LhDpQqw==D0VqpsDFkGSWCC98' },
-                    contentType: 'application/json',
-                    success: function (result) {
+
+setup = () => {
+    $("#convert").click(() => {
+        let fromCurrency = $('#fromCurrency').val()
+        let toCurrency = $("#toCurrency").val()
+        let amount = $('#amount').val()
+        // console.log("convert button");
+        // console.log(fromCurrency);
+        // console.log(toCurrency);
+        // console.log(amount);
+        $.ajax({
+            method: 'GET',
+            url: `https://api.api-ninjas.com/v1/convertcurrency?want=${fromCurrency}&have=${toCurrency}&amount=${amount}`,
+            headers: { 'X-Api-Key': 'bqWH6covDtf0E84LhDpQqw==D0VqpsDFkGSWCC98' },
+            contentType: 'application/json',
+            success: function (result) {
+                firebase.auth().onAuthStateChanged((user) => {
+                    if (user) {
                         console.log(result);
                         $('#result').html(`${amount} ${fromCurrency} = ${result.new_amount} ${toCurrency}`)
-                    },
-                    error: function ajaxError(jqXHR) {
-                        console.error('Error: ', jqXHR.responseText);
+                    } else {
+                        alert("Please Log In to process the page.");
                     }
                 });
-            })
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+        });
+    })
 
-            $.ajax({
-                method: 'GET',
-                url: 'https://api.api-ninjas.com/v1/interestrate',
-                headers: { 'X-Api-Key': 'bqWH6covDtf0E84LhDpQqw==D0VqpsDFkGSWCC98' },
-                contentType: 'application/json',
-                success: function (result) {
-                    console.log(result);
-                },
-                error: function ajaxError(jqXHR) {
-                    console.error('Error: ', jqXHR.responseText);
-                }
-            });
-
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/interestrate',
+        headers: { 'X-Api-Key': 'bqWH6covDtf0E84LhDpQqw==D0VqpsDFkGSWCC98' },
+        contentType: 'application/json',
+        success: function (result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
         }
-    } else {
-        alert("Please Log In to process the page.");
-    }
-});
+    });
+
+}
 
 $(document).ready(setup)
